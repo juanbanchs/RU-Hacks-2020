ff05fb4097ab0eaf0d1fc8c24d18d9cf5f89f541
juanbanchs <jsbanchs@gmail.com>
2020-05-16 13:55:26 -0700

    Test to show Tanya

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask
-# import requests
+from flask import Flask, json
+import requests
 
 app = Flask(__name__)
 
@@ -7,9 +7,13 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 
-    # data =requests.get("https://api.spoonacular.com/recipes/search?query=potato&number=2&apiKey=851e31b033fb47518e40cbd5a6b6019a")
-    # potato = "Hi"
-    return '<h2>WOOOHOOOOOOOO2<h2>'
+    data = json.loads(requests.get(
+        "https://api.spoonacular.com/recipes/search?query=potato&number=2&apiKey=851e31b033fb47518e40cbd5a6b6019a").content.decode('utf-8'))
+
+    # data 2 = json.loads({"results":[{"id":310822,"title":"Potato Cauliflower Parsnip Soup with a Pancetta Potato Hash","readyInMinutes":90,"servings":4,"sourceUrl":"http://www.foodnetwork.com/recipes/nikki-dinki/potato-cauliflower-parsnip-soup-with-a-pancetta-potato-hash-recipe.html","openLicense":0,"image":"potato-cauliflower-parsnip-soup-with-a-pancetta-potato-hash-310822.jpeg"},{"id":202914,"title":"Smoky Sweet Potato Mashed Potato Bake","readyInMinutes":50,"servings":8,"sourceUrl":"http://www.myrecipes.com/recipe/smoky-mashed-sweet-potato-bake-50400000118033/","openLicense":0,"image":"smoky-sweet-potato-mashed-potato-bake-202914.jpg"}],"baseUri":"https://spoonacular.com/recipeImages/","offset":0,"number":2,"totalResults":851,"processingTimeMs":424,"expires":1589872815358,"isStale":false})
+    # # potato = "Hi"
+    # potato = "hi"
+    return data
 
 
 # if __name__ == "__main__":
